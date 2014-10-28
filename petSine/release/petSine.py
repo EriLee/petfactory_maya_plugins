653a9f4bba621ea15f27380546383040bf4707ff
Johan Borgström <johan@petfactory.se>
2014-10-28 10:37:27 +0100

    add angle and angle scale

--- petSine/release/petSine.py
+++ petSine/release/petSine.py
@@ -2,155 +2,138 @@ import sys, math
 from maya import OpenMaya, OpenMayaMPx
 
 # The name of the node.
-kPluginNodeName = 'petSine' 
-# Where this node will be found in the Maya UI.
+node_name = 'petSine' 
 
-kPluginNodeClassify = 'utility/general'
+node_classify = 'utility/general'
 
 # A unique ID associated to this node type.
 # Plugs for internal use only can use 0 - 0x7ffff.
-# Plugs that AD ships in th edev kit uses 0x80000 - 0xfffff
-# if you plan to distribute the plug, get an ID from AD  
-kPluginNodeId = OpenMaya.MTypeId(0x00001) 
+node_id = OpenMaya.MTypeId(0x00001)
 
-##########################################################
-# Plug-in 
-##########################################################
-class myNode(OpenMayaMPx.MPxNode):
-    # Static variables which will later be replaced by the node's attributes.
-    input_value = OpenMaya.MObject()
-    frequency = OpenMaya.MObject()
-    scale = OpenMaya.MObject()
-    out_sine = OpenMaya.MObject()
-    
+class petSine(OpenMayaMPx.MPxNode):
+
+    # define attrs. This will hold a ref to the MObj that are created in the nodeInitializer
+    in_angle = OpenMaya.MObject()
+    in_angle_scale = OpenMaya.MObject()
+    out_result = OpenMaya.MObject()
+
     def __init__(self):
-        ''' Constructor. '''
         OpenMayaMPx.MPxNode.__init__(self)
-        
-    #def compute(self, pPlug, pDataBlock):
-    def compute(self, plug, data):
-        '''
-        Node computation method.
-            - plug: A connection point related to one of our node attributes (could be an input or an output)
-            - data: Contains the data on which we will base our computations.
-        '''
-        
-        if(plug == myNode.out_sine):
-            
-            # Obtain the data handles for each attribute
-            input_data = data.inputValue(myNode.input_value)
-            scale_data = data.inputValue(myNode.scale)
-            frequency_data = data.inputValue(myNode.frequency)
-            out_data = data.outputValue(myNode.out_sine)
-            
-            # Extract the actual value associated to our sample input attribute (we have defined it as a float)
-            input_val = input_data.asFloat()
-            scale_val = scale_data.asFloat()
-            frequency_val = frequency_data.asFloat()
 
-            # perform the desired computation
-            #angle = 6.2831853 * (input_val/frequency_val)
-            angle = (6.2831853 / frequency_val) * input_val
-            sin_result = math.sin(angle) * scale_val
-            
-            # Set the output value.
-            out_data.setFloat(sin_result)
-            
-            # Mark the output data handle as being clean; it need not be computed given its input.
-            out_data.setClean()
-             
+    def compute(self, plug, data_block):
+
+        # only output plugs will be passed to the compute method
+        # we could do different computes for different plugs
+        if plug == petBaseClass.out_result:
+
+            #--------------------
+            # INPUT
+            #--------------------
+            # get the datahandle from the data block
+            in_angle_datahandle = data_block.inputValue(petBaseClass.in_angle)
+            in_angle_scale_datahandle = data_block.inputValue(petBaseClass.in_angle_scale)
+
+            # get the data (float in this case) from the data handle
+            in_angle_value = in_angle_datahandle.asFloat()
+            in_angle_scale_value = in_angle_scale_datahandle.asFloat()
+
+            #--------------------
+            # COMPUTE
+            #--------------------
+            out_result_value = in_angle_value * in_angle_scale_value
+
+            #--------------------
+            # OUTPUT
+            #--------------------
+            # get the datahandle from the data block
+            out_result_datahandle = data_block.outputValue(petBaseClass.out_result)
+            out_result_datahandle.setFloat(out_result_value)
+
+            # mark the plug clean
+            data_block.setClean(plug)
+
+        # optionally we could return unknown here, sine we expect only one output 
+        # plug specified to be passed to the compute method
         else:
             return OpenMaya.kUnknownParameter
 
-##########################################################
-# Plug-in initialization.
-##########################################################
+
 def nodeCreator():
-    ''' Creates an instance of our node class and delivers it to Maya as a pointer. '''
-    #sys.stdout.write('apa\n')
-    return OpenMayaMPx.asMPxPtr(myNode())
+    # create an instance and return a pointer MObj to it
+    return OpenMayaMPx.asMPxPtr(petSine())
 
 def nodeInitializer():
-    ''' Defines the input and output attributes as static variables in our plug-in class. '''
-    
-    # The following MFnNumericAttribute function set will allow us to create our attributes.
-    nAttr = OpenMaya.MFnNumericAttribute()
+    # create a function set for numeric attributes, will be used as a
+    # factory to create attributes, they will be returned as MObj
+    mfn_attr = OpenMaya.MFnNumericAttribute()
     kFloat = OpenMaya.MFnNumericData.kFloat
-    
-    #==================================
-    # INPUT NODE ATTRIBUTE(S)
-    #==================================
 
-    # input_value
-    myNode.input_value = nAttr.create('input', 'in', kFloat, 0.0)
-    nAttr.setWritable(True)
-    nAttr.setStorable(True) 
-    nAttr.setHidden(False)
-    myNode.addAttribute(myNode.input_value)
+    #--------------------
+    # INPUT
+    #--------------------
+    # create an attr. params: longname, shortname, datatype, default
+    petBaseClass.in_angle = mfn_attr.create('angle', 'a', kFloat, 0.0)
+    # set the properties of the attr
+    mfn_attr.setReadable(1)
+    mfn_attr.setWritable(1)
+    mfn_attr.setStorable(1)
+    mfn_attr.setKeyable(1)
 
-    # frequency
-    myNode.frequency = nAttr.create('frequency', 'fr', kFloat, 48.0)
-    nAttr.setWritable(True)
-    nAttr.setStorable(True) 
-    nAttr.setHidden(False)
-    nAttr.setMin(0.0001) 
-    myNode.addAttribute(myNode.frequency)
+    # create an attr. params: longname, shortname, datatype, default
+    petBaseClass.in_angle_scale = mfn_attr.create('angleScale', 'as', kFloat, 0.0)
+    # set the properties of the attr
+    mfn_attr.setReadable(1)
+    mfn_attr.setWritable(1)
+    mfn_attr.setStorable(1)
+    mfn_attr.setKeyable(1)
 
-    # scale
-    myNode.scale = nAttr.create('scale', 'sc', kFloat, 10.0)
-    nAttr.setWritable(True)
-    nAttr.setStorable(True) 
-    nAttr.setHidden(False)
-    myNode.addAttribute(myNode.scale)
-    
-    #==================================
-    # OUTPUT NODE ATTRIBUTE(S)
-    #==================================
-    myNode.out_sine = nAttr.create('outSine', 'o', kFloat)
-    nAttr.setStorable(False)
-    nAttr.setWritable(False)
-    nAttr.setReadable(True)
-    nAttr.setHidden(False)
-    myNode.addAttribute(myNode.out_sine)
-    
-    #==================================
-    # NODE ATTRIBUTE DEPENDENCIES
-    #==================================
-    # If sampleInAttribute changes, the sampleOutAttribute data must be recomputed.
-    myNode.attributeAffects(myNode.input_value, myNode.out_sine)
-    myNode.attributeAffects(myNode.frequency, myNode.out_sine)
-    myNode.attributeAffects(myNode.scale, myNode.out_sine)
-    
-    
-def initializePlugin( mobject ):
-    ''' Initialize the plug-in '''
+
+    #--------------------
+    # OUTPUT
+    #--------------------
+    # create an attr. params: longname, shortname, datatype, default
+    # NOTE that output value has no default value (it will be computed)
+    petBaseClass.out_result = mfn_attr.create('result', 'r', kFloat)
+    # set the properties of the attr
+    # NOTE only readable
+    mfn_attr.setReadable(1)
+    mfn_attr.setWritable(0)
+    mfn_attr.setStorable(0)
+    mfn_attr.setKeyable(0)
+
+
+    #--------------------
+    # ADD ATTR TO NODE
+    #--------------------
+    petBaseClass.addAttribute(petBaseClass.in_angle)
+    petBaseClass.addAttribute(petBaseClass.in_angle_scale)
+    petBaseClass.addAttribute(petBaseClass.out_result)
+
+    #--------------------
+    # SETUP DEPENDENCY
+    #--------------------
+    # which attributes needs to be updated if an attribute is changed
+    petBaseClass.attributeAffects(petBaseClass.in_angle, petBaseClass.out_result)
+    petBaseClass.attributeAffects(petBaseClass.in_angle_scale, petBaseClass.out_result)
+
+def initializePlugin(mobject):
     mplugin = OpenMayaMPx.MFnPlugin(mobject)
     try:
-        mplugin.registerNode( kPluginNodeName, kPluginNodeId, nodeCreator,
-                              nodeInitializer, OpenMayaMPx.MPxNode.kDependNode, kPluginNodeClassify )
+        mplugin.registerNode(   node_name,
+                                node_id,
+                                nodeCreator,
+                                nodeInitializer,
+                                OpenMayaMPx.MPxNode.kDependNode,
+                                node_classify)
     except:
-        sys.stderr.write( 'Failed to register node: ' + kPluginNodeName )
+        sys.stderr.write( 'Failed to register node: ' + node_name )
         raise
-    
-def uninitializePlugin( mobject ):
-    ''' Uninitializes the plug-in '''
-    mplugin = OpenMayaMPx.MFnPlugin( mobject )
+
+
+def uninitializePlugin(mobject):
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
     try:
-        mplugin.deregisterNode( kPluginNodeId )
+        mplugin.deregisterNode(node_id)
     except:
-        sys.stderr.write( 'Failed to deregister node: ' + kPluginNodeName )
+        sys.stderr.write( 'Failed to deregister node: ' + node_name )
         raise
-
-##########################################################
-# Sample usage.
-##########################################################
-''' 
-# Copy the following lines and run them in Maya's Python Script Editor:
-
-import maya.cmds as cmds
-
-cmds.loadPlugin( 'sampleDGNode.py' )
-cmds.createNode( 'myNodeName' )
-# ...
-
-'''
